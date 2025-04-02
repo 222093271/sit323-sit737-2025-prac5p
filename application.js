613c81f4927da735196dfbaefab2592df9a4c13f
222093271 <118093608+222093271@users.noreply.github.com>
2025-04-02 04:56:05 +0000

    Restart

--- application.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const app = express();
-const port = 3000;
-
-app.use(express.static('public'));
-
-app.listen(port, () => {
-  console.log(`🌐 App running at http://localhost:${port}`);
-});
